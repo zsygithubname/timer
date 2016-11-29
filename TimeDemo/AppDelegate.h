2005dfe7e2251420211f6310bf5bcbc4ecf4d615
zsy <zsy@zsy.local>
2016-11-29 10:14:10 +0800

    Initial Commit

---
+++ TimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeDemo
+//
+//  Created by zsy on 2016/11/29.
+//  Copyright © 2016年 zsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeDemo
+//
+//  Created by zsy on 2016/11/29.
+//  Copyright © 2016年 zsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
